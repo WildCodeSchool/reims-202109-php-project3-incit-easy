13923f1b1cafa5ddb93752f2a249800b0df4c6ec
Cyl <labetowiez@aol.fr>
2021-12-08 14:32:12 +0100

    did the progress bar

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,10 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+const bar = document.getElementById('bar');
+const percent = document.getElementById('bar-percent');
+
+const rotation = parseInt(45 + (percent.innerHTML * 1.8), 10);
+
+bar.style.transform = `rotate(${rotation}deg)`;
