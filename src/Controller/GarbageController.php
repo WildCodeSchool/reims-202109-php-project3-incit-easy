c2e0eebbac51d8212a46f0136bf9ea851894c7ce
Antoine Leclabart <antoine.leclabart@gmail.com>
2021-12-08 11:23:30 +0100

    forwarded data to view

--- src/Controller/GarbageController.php
+++ src/Controller/GarbageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GarbageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class GarbageController extends AbstractController
 {
     #[Route('/garbage', name: 'garbage')]
-    public function index(): Response
+    public function index(GarbageRepository $garbageRepository): Response
     {
+        $allGarbage = $garbageRepository->findById(1);
+        $totalGarbage = $allGarbage['nonRecycledWaste'] + $allGarbage['recycledWaste'];
+        
         return $this->render('garbage/index.html.twig', [
-            'controller_name' => 'GarbageController',
+            'garbage' => $allGarbage,
+            'totalGarbage' => $totalGarbage,
         ]);
     }
 }
