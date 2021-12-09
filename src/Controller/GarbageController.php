c8df7c9b12f1f35582d8072e3d5859854be8a9aa
Antoine Leclabart <antoine.leclabart@gmail.com>
2021-12-09 16:19:31 +0100

    separator and font

--- src/Controller/GarbageController.php
+++ src/Controller/GarbageController.php
@@ -6,13 +6,14 @@ use App\Repository\GarbageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Garbage;
 
 class GarbageController extends AbstractController
 {
     #[Route('/garbage', name: 'garbage')]
     public function index(GarbageRepository $garbageRepository): Response
     {
-        $garbage = $garbageRepository->findOneById(4);
+        $garbage = $garbageRepository->findOneById(rand(1, 5));
         $totalGarbage =  $garbage->getNonRecycledWaste() + $garbage->getRecycledWaste();
         return $this->render('garbage/index.html.twig', [
             'garbage' => $garbage,
