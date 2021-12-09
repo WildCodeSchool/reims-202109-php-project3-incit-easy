b6751f3d321ef8f2bc421c04e0b6f03d4c47c9ec
Antoine Leclabart <antoine.leclabart@gmail.com>
2021-12-09 11:32:29 +0100

    final arc dashboard

--- assets/app.js
+++ assets/app.js
@@ -10,14 +10,12 @@ import './bootstrap';
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
-const bar = document.getElementById('bar');
 const percent = document.getElementById('bar-percent');
 
 const rotation = parseInt(Math.min((percent.innerHTML * 1.8), 360), 10);
 
-if (parseInt(percent.innerHTML, 10) >= 100) {
-    bar.style.borderTopColor = 'red';
-    bar.style.borderLeftColor = 'red';
-}
+document.documentElement.style.setProperty('--rotation', `${rotation}deg`);
 
-bar.style.transform = `rotate(${rotation}deg)`;
+if (percent.innerHTML < 100) {
+    document.documentElement.style.setProperty('--final-color', '#eee');
+}
