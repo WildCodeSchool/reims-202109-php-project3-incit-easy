ef9c78c8fd5b34b75ad118824213fc8487298dcb
Cyl <labetowiez@aol.fr>
2021-12-15 15:51:25 +0100

    changed entity contructor for garbage

--- src/DataFixtures/GarbageFixtures.php
+++ src/DataFixtures/GarbageFixtures.php
@@ -5,9 +5,10 @@ namespace App\DataFixtures;
 use App\Entity\Garbage;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class GarbageFixtures extends Fixture
+class GarbageFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
@@ -16,7 +17,6 @@ class GarbageFixtures extends Fixture
             $garbage = new Garbage();
             $garbage->setRecycledWaste($i);
             $garbage->setNonRecycledWaste($i + (rand(1, 5)));
-            $garbage->setCreatedAt(new DateTimeImmutable());
             $manager->persist($garbage);
         }
         $manager->flush();
