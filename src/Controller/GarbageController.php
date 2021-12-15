2014026a9d3a04ff0e44b47e37c3332fb8e1b5a8
Cyl <labetowiez@aol.fr>
2021-12-15 16:15:52 +0100

    added security to able only users to access garbage page

--- src/Controller/GarbageController.php
+++ src/Controller/GarbageController.php
@@ -13,6 +13,8 @@ class GarbageController extends AbstractController
     #[Route('/garbage', name: 'garbage')]
     public function index(GarbageRepository $garbageRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
         $garbages = $garbageRepository->findAll();
         $garbage = $garbages[rand(0, count($garbages) - 1)];
         $totalGarbage =  $garbage->getNonRecycledWaste() + $garbage->getRecycledWaste();
