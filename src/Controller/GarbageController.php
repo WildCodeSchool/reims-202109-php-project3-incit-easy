1b8735721953dad842eaf41a2f18614209ec5cfe
Cyl <labetowiez@aol.fr>
2021-12-16 09:44:32 +0100

    change routes and security

--- src/Controller/GarbageController.php
+++ src/Controller/GarbageController.php
@@ -7,20 +7,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Garbage;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 
+#[Route('/garbage', name: 'garbage_')]
 class GarbageController extends AbstractController
 {
-    #[Route('/garbage', name: 'garbage')]
+    #[Route('/', name: 'index')]
     public function index(GarbageRepository $garbageRepository): Response
     {
-        $this->denyAccessUnlessGranted('ROLE_USER');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        return $this->render('garbage/index.html.twig');
+    }
 
-        $garbages = $garbageRepository->findAll();
-        $garbage = $garbages[rand(0, count($garbages) - 1)];
-        $totalGarbage =  $garbage->getNonRecycledWaste() + $garbage->getRecycledWaste();
-        return $this->render('garbage/index.html.twig', [
+    #[Route('/{id}', name: 'show')]
+    public function show(Garbage $garbage): Response
+    {
+        $user = $this->getUser();
+        if ($user !== $garbage->getUser()) {
+            throw new AccessDeniedException("Vous n'avez pas le droit d'accéder à ces données.");
+        }
+        return $this->render('garbage/show.html.twig', [
             'garbage' => $garbage,
-            'totalGarbage' => $totalGarbage,
         ]);
     }
 }
