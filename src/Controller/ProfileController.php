5a226cb2de507ec68878a810714c03c2979f4bd8
Cyl <labetowiez@aol.fr>
2021-12-16 10:28:48 +0100

    added render of list garbage in the profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/profile', name: 'profile_')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'profile')]
-    public function index(): Response
+    #[Route('/{id}', name: 'show')]
+    public function show(User $user): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
