6cad0a5cf37b244c666d349f5caade741314420f
CorentinChalons <chalons.corentin@gmail.com>
2022-01-05 14:48:20 +0100

    Change entity garbage and entity and _list.html.twig

---
+++ migrations/Version20220105133347.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220105133347 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE garbage ADD type VARCHAR(255) NOT NULL, ADD weight DOUBLE PRECISION NOT NULL, DROP recycled_waste, DROP non_recycled_waste');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE garbage ADD non_recycled_waste DOUBLE PRECISION NOT NULL, DROP type, CHANGE weight recycled_waste DOUBLE PRECISION NOT NULL');
+    }
+}
