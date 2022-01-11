11ef85a2281ca31f56a74ce2431197d6c55ef4d1
CorentinChalons <chalons.corentin@gmail.com>
2022-01-11 14:53:00 +0100

    make the CRUD for the community page

---
+++ src/Controller/PostController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Entity\User;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/post')]
+class PostController extends AbstractController
+{
+    #[Route('/', name: 'post_index', methods: ['GET', 'POST'])]
+    public function index(
+        Request $request,
+        PostRepository $postRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $post = new Post();
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $date = new DateTimeImmutable();
+            $user = (User)($this->getUser());
+            $post->setCreatedAt($date)->setUser($user);
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+            'postForm' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'post_show', methods: ['GET'])]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'post_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('post_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'post_delete', methods: ['POST'])]
+    public function delete(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $post->getId(), (string)$request->request->get('_token'))) {
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('post_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('message', TextType::class, ['attr' =>
+            ['placeholder' => 'un message']
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
